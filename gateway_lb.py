52e516123633303732bb8d4d2b5cd81b20248fe0
andrezuu <sanchezandres8203@gmail.com>
2025-04-23 23:11:00 -0400

    gateway

--- gateway_lb.py
+++ gateway_lb.py
@@ -12,29 +12,32 @@ print("Gateway escuchando en puerto 9000...")
 
 while True:
     conn, addr = gateway_socket.accept()
-    data = conn.recv(1024).decode()
 
-    print(f"Gateway recibió: {data}")
+    while True:
+        print(f'Gateway en espera...')
+        data = conn.recv(1024).decode()
 
-    backend = backends[counter % len(backends)]
-    counter += 1
+        print(f"Gateway recibió: {data}")
 
-    try:
-        with socket.socket() as backend_socket:
-            backend_socket.connect(backend)
-            backend_socket.sendall(data.encode())
-            response = backend_socket.recv(1024)
-    except Exception as e:
-        print(f"Error al conectar con backend {backend}: {e}")
-        other_backend = backends[(counter) % len(backends)] # Ira al siguiente backend si uno falla
-        print(f"Intentando con el siguiente backend: {other_backend}")
+        backend = backends[counter % len(backends)]
+        counter += 1
         try:
             with socket.socket() as backend_socket:
-                backend_socket.connect(other_backend)
+                backend_socket.connect(backend)
                 backend_socket.sendall(data.encode())
                 response = backend_socket.recv(1024)
         except Exception as e:
-            response = f"Error: Ningún backend disponible ({e})".encode()
+            print(f"Error al conectar con backend {backend}: {e}")
+            other_backend = backends[(counter) % len(backends)] # Ira al siguiente backend si uno falla
+            print(f"Intentando con el siguiente backend: {other_backend}")
+            try:
+                with socket.socket() as backend_socket:
+                    backend_socket.connect(other_backend)
+                    backend_socket.sendall(data.encode())
+                    response = backend_socket.recv(1024)
+            except Exception as e:
+                response = f"Error: Ningún backend disponible ({e})".encode()
 
-    conn.sendall(response)
-    conn.close()
+        conn.sendall(response)
+
+    
